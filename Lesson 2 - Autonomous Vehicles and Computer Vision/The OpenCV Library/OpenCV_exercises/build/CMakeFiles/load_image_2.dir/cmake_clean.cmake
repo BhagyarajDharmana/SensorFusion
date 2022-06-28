29b9ead3c25a8eede4a3f6b4516187a50179773a
BhagyarajDharmana <50155747+BhagyarajDharmana@users.noreply.github.com>
2022-06-28 15:31:06 +0200

    Add files via upload

---
+++ Lesson 2 - Autonomous Vehicles and Computer Vision/The OpenCV Library/OpenCV_exercises/build/CMakeFiles/load_image_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/load_image_2.dir/src/load_image_2.cpp.o"
+  "load_image_2"
+  "load_image_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/load_image_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
